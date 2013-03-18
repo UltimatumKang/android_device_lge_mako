b62cc2f0eb29047f852e11b62956d9cfb63bc2f5
Daniel <daxx4182@gmail.com>
2013-03-18 14:42:41 +0800

    TricksterMod
    Signed-off-by: Daniel <daxx4182@gmail.com>

--- device.mk
+++ device.mk
@@ -102,6 +102,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	device/lge/mako/gps.conf:system/etc/gps.conf
 
+# Trickster Mod
+PRODUCT_PACKAGES += \
+    TM
+
 # NFC packages
 PRODUCT_PACKAGES += \
      libnfc-nci \
