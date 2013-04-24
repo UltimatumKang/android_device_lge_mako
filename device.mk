c3c0232e4c06eec26a9eaa9138108583301334c4
Daniel <daxx4182@gmail.com>
2013-04-24 22:54:02 +0800

    Revert "F**K Tacons, let's just use prebuilt"
    This reverts commit aa0803b991ac2bf87089adad1410a46505aaa351.

--- device.mk
+++ device.mk
@@ -35,11 +35,6 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     charger
 
-LOCAL_KERNEL := device/lge/mako/kernel
-
-PRODUCT_COPY_FILES := \
-	$(LOCAL_KERNEL):kernel
-
 # Live Wallpapers
 PRODUCT_PACKAGES += \
         LiveWallpapers \
