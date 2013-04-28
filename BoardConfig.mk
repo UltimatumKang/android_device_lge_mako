f6a57f009f97585a7367ebf8fa0bdef8285e3c2c
Daniel <daxx4182@gmail.com>
2013-04-28 16:37:33 +0800

    Better Optimizations
    From CarbonDev and VanirAOSP

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,15 +14,16 @@
 # limitations under the License.
 #
 
-TARGET_EXTRA_CFLAGS := -mtune=cortex-a9 -mcpu=cortex-a9
+TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mcpu=cortex-a9 -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mcpu=cortex-a9 -mfloat-abi=softfp
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
-
-TARGET_ARM_OPTIMIZATIONS := true
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_CPU_VARIANT := cortex-a9
 
 # Krait optimizations
 TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
